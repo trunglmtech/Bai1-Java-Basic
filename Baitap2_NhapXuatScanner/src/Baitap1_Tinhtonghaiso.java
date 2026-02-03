e92c91cec4e42ba199e63cca76816d90321dd2b3
trunglmtech <0602trungle95@gmail.com>
2026-02-03 16:23:38 +0700

    Nhập dữ liệu từ bàn phím

--- Baitap2_NhapXuatScanner/src/Baitap1_Tinhtonghaiso.java
+++ Baitap2_NhapXuatScanner/src/Baitap1_Tinhtonghaiso.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 public class Baitap1_Tinhtonghaiso {
 
 	public static void main(String[] args) {
-		
+		// Nhập dữ liệu từ bàn phím 
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập số thứ nhất: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Nhập số thứ hai: ");
+        double num2 = scanner.nextDouble();
 
 	}
 
