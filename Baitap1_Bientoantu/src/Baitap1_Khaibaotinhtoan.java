8552362d2624497a95ea69d742a9fced8571fee3
trunglmtech <0602trungle95@gmail.com>
2026-02-03 16:10:34 +0700

    Tính toán

--- Baitap1_Bientoantu/src/Baitap1_Khaibaotinhtoan.java
+++ Baitap1_Bientoantu/src/Baitap1_Khaibaotinhtoan.java
@@ -2,8 +2,14 @@
 public class Baitap1_Khaibaotinhtoan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 1. Khai báo các biến
+		int a = 15;
+        int b = 4;
+        double c = 3.5;
+        // 2. Tính toán
+        int tong = a + b;
+        double thuong = a / c;
+        int phanDu = a ;
 	}
 
 }
