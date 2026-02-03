4b83e5f9e13e948334014c831271bfadf332b214
trunglmtech <0602trungle95@gmail.com>
2026-02-03 16:29:42 +0700

    Điều kiện BMI và in ra kết quả

--- Baitap2_NhapXuatScanner/src/Baitap2_TinhchisoBMI.java
+++ Baitap2_NhapXuatScanner/src/Baitap2_TinhchisoBMI.java
@@ -19,7 +19,18 @@ public class Baitap2_TinhchisoBMI {
         System.out.printf("Cân nặng: PT.1f kgPTn", weight);
         System.out.printf("Chiều cao: PT.2f mPTn", height);
         System.out.printf("Chỉ số BMI: PT.2fPTn", bmi);
-
+        // Điều kiện BMI và in ra kết quả
+        if (bmi < 18.5) {
+            System.out.println("Phân loại: Thiếu cân");
+        } else if (bmi < 25) {
+            System.out.println("Phân loại: Bình thường");
+        } else if (bmi < 30) {
+            System.out.println("Phân loại: Thừa cân");
+        } else {
+            System.out.println("Phân loại: Béo phì");
+        }
+        
+        scanner.close();
 	}
 
 }
