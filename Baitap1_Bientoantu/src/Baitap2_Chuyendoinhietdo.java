ec4e5e5f52c0a04b042d0a3344da9a197d4507f5
trunglmtech <0602trungle95@gmail.com>
2026-02-03 16:13:21 +0700

    In kết quả chuyển đổi

--- Baitap1_Bientoantu/src/Baitap2_Chuyendoinhietdo.java
+++ Baitap1_Bientoantu/src/Baitap2_Chuyendoinhietdo.java
@@ -5,6 +5,8 @@ public class Baitap2_Chuyendoinhietdo {
 		// Khai báo biến và công thức chuyển
 		double celsius = 25.0;
         double fahrenheit = (celsius * 9/5) + 32;
+        // In kết quả chuyển đổi
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
 
 	}
 
