2bfcf610813152011073ace8ca61819b67f0be7e
trunglmtech <0602trungle95@gmail.com>
2026-02-03 16:29:03 +0700

    Nhập dữ liệu chiều cao cân nặng từ bàn phím và tính toán và in ra

--- Baitap2_NhapXuatScanner/src/Baitap2_TinhchisoBMI.java
+++ Baitap2_NhapXuatScanner/src/Baitap2_TinhchisoBMI.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 public class Baitap2_TinhchisoBMI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Nhập dữ liệu chiều cao cân nặng từ bàn phím và tính toán và in ra
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("=== TÍNH CHỈ SỐ BMI ===");
+        System.out.print("Nhập cân nặng (kg): ");
+        double weight = scanner.nextDouble();
+        
+        System.out.print("Nhập chiều cao (m): ");
+        double height = scanner.nextDouble();
+        
+        double bmi = weight / (height * height);
+        
+        System.out.printf("PTn=== KẾT QUẢ ===PTn");
+        System.out.printf("Cân nặng: PT.1f kgPTn", weight);
+        System.out.printf("Chiều cao: PT.2f mPTn", height);
+        System.out.printf("Chỉ số BMI: PT.2fPTn", bmi);
 
 	}
 
