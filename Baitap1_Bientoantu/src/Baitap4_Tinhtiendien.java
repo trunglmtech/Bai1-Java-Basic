599e6dde31abaca5330fa542a56a9cc6b735ce7d
trunglmtech <0602trungle95@gmail.com>
2026-02-03 16:18:48 +0700

    Điều kiện và tính tiền điện

--- Baitap1_Bientoantu/src/Baitap4_Tinhtiendien.java
+++ Baitap1_Bientoantu/src/Baitap4_Tinhtiendien.java
@@ -5,7 +5,18 @@ public class Baitap4_Tinhtiendien {
 			// Khai báo dữ liệu điện
 		 int soDien = 250;  // kWh
 	     double tienDien = 0;
-
+	     // Điều kiện và tính tiền điện
+	     if (soDien <= 50) {
+	            tienDien = soDien * 1678;
+	        } else if (soDien <= 100) {
+	            tienDien = 50 * 1678 + (soDien - 50) * 1734;
+	        } else if (soDien <= 200) {
+	            tienDien = 50 * 1678 + 50 * 1734 + (soDien - 100) * 2014;
+	        } else if (soDien <= 300) {
+	            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (soDien - 200) * 2536;
+	        } else {
+	            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (soDien - 300) * 2834;
+	        }
 	}
 
 }
