71da57ce6dedd5fd10feae60e02ef72217f6e78e
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:15:34 +0700

    Chia bật thang if - else tính tiền điện + dồn

--- Baitap2_NhapXuatScanner/src/Baitap1_Tinhtiendien.java
+++ Baitap2_NhapXuatScanner/src/Baitap1_Tinhtiendien.java
@@ -11,6 +11,21 @@ public class Baitap1_Tinhtiendien {
 	        int soDien = sc.nextInt();
 
 	        double tienDien = 0;
+	    // Chia bật thang if - else tính tiền điện + dồn
+	        if (soDien <= 50) {
+	            tienDien = soDien * 1678;
+	        } else if (soDien <= 100) {
+	            tienDien = 50 * 1678 + (soDien - 50) * 1734;
+	        } else if (soDien <= 200) {
+	            tienDien = 50 * 1678
+	                     + 50 * 1734
+	                     + (soDien - 100) * 2014;
+	        } else {
+	            tienDien = 50 * 1678
+	                     + 50 * 1734
+	                     + 100 * 2014
+	                     + (soDien - 200) * 2536;
+	        }
 
 	}
 
