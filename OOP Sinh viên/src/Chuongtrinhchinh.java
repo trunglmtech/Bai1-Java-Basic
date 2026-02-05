bcad373ccfc496f714d3bfb7ef2ef08dd4d1e9be
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:32:04 +0700

    cấp/gán dữ liệu cụ thể

--- OOP Sinh viên/src/Chuongtrinhchinh.java
+++ OOP Sinh viên/src/Chuongtrinhchinh.java
@@ -4,7 +4,11 @@ public class Chuongtrinhchinh {
 	public static void main(String[] args) {
 		// Tạo mới 1 sinh viên
 		SinhVien sv1 = new SinhVien();
-
+		// cấp/gán dữ liệu cụ thể
+				sv1.mssv="66112233";
+				sv1.ten="Phan Thị";
+				sv1.tuoi=18;
+				sv1.hocBai("Lập trình Java");
 	}
 
 }
