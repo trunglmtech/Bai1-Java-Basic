6e55b0b9edbb774299aa7ef234e8dbcb60810ec1
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:31:24 +0700

    Tạo mới 1 sinh viên

--- OOP Sinh viên/src/Chuongtrinhchinh.java
+++ OOP Sinh viên/src/Chuongtrinhchinh.java
@@ -2,7 +2,8 @@
 public class Chuongtrinhchinh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Tạo mới 1 sinh viên
+		SinhVien sv1 = new SinhVien();
 
 	}
 
