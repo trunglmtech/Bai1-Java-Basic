309c220ef945335744d68477a50a2ccefb66e453
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:30:09 +0700

    Định nghĩa các phương thức / hàm

--- OOP Sinh viên/src/Sinhvien.java
+++ OOP Sinh viên/src/Sinhvien.java
@@ -6,7 +6,10 @@ public class Sinhvien {
 		String ten;
 		String mssv;
 		int tuoi;
-
+		// Định nghĩa các phương thức / hàm
+			void hocBai(String monhoc) {
+			System.out.print("Bạn " + ten + " đang học môn " + monhoc);
+		}
 	}
 
 }
