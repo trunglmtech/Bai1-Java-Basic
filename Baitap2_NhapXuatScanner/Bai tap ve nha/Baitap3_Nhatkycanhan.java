a389350b276676a4e91efdd35f8dc15ef6380edb
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:25:02 +0700

    Tạo file import các lớp

---
+++ Baitap2_NhapXuatScanner/Bai tap ve nha/Baitap3_Nhatkycanhan.java
@@ -0,0 +1,14 @@
+//Import các lớp
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Baitap3_Nhatkycanhan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
