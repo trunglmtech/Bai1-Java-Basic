f20c808a1577e5d2a64ee624b06d30ca19345f38
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:14:35 +0700

    Nhập số điện & khởi tạo tiền điện = 0

--- Baitap2_NhapXuatScanner/src/Baitap1_Tinhtiendien.java
+++ Baitap2_NhapXuatScanner/src/Baitap1_Tinhtiendien.java
@@ -1,9 +1,16 @@
 // Import lớp scanner
+import java.util.Scanner;
 
 public class Baitap1_Tinhtiendien {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Nhập số điện
+			Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Nhap so dien: ");
+	        int soDien = sc.nextInt();
+
+	        double tienDien = 0;
 
 	}
 
