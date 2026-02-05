1c769d88df9bdbd68dca1ea83263e0f36695c666
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:21:05 +0700

    Khai báo dữ liệu tỉ giá tiền hiện tại và nhập số tiền cần đổi

--- Baitap2_NhapXuatScanner/Bai tap ve nha/Baitap2_Quydoitien.java
+++ Baitap2_NhapXuatScanner/Bai tap ve nha/Baitap2_Quydoitien.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class Baitap2_Quydoitien {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Khai báo dữ liệu tỉ giá tiền hiện tại và nhập số tiền cần đổi
+			Scanner sc = new Scanner(System.in);
+
+	        double tiGia = 26000;
+
+	        System.out.println("1.USD > VND");
+	        System.out.print("Chon kieu quy doi: ");
+	        int chon = sc.nextInt();
+
+	        System.out.print("Nhap so tien: ");
+	        double soTien = sc.nextDouble();
 
 	}
 
