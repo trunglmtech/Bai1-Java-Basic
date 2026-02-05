4face47506113c4a584f03824e357202b0433de5
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:26:06 +0700

    Ghi nhật ký

--- Baitap2_NhapXuatScanner/Bai tap ve nha/Baitap3_Nhatkycanhan.java
+++ Baitap2_NhapXuatScanner/Bai tap ve nha/Baitap3_Nhatkycanhan.java
@@ -12,7 +12,16 @@ public class Baitap3_Nhatkycanhan {
 
 	        System.out.print("Nhap noi dung nhat ky: ");
 	        String noiDung = sc.nextLine();
+	    // Ghi nhật ký
+	        try {
+	            FileWriter fw = new FileWriter("nhatky.txt", true);
+	            fw.write(noiDung + "\n");
+	            fw.close();
 
+	            System.out.println("Da ghi nhat ky thanh cong");
+	        } catch (IOException e) {
+	            System.out.println("Loi khi ghi file");
+	        }
 	}
 
 }
