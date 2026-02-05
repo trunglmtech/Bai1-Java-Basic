0c5252a9f0b89d6a98b6fbfe1a770a2bea005859
trunglmtech <0602trungle95@gmail.com>
2026-02-05 16:09:29 +0700

    Nhập thông tin các môn

--- Baitap2_NhapXuatScanner/src/Baitap3_QuanlySVFile.java
+++ Baitap2_NhapXuatScanner/src/Baitap3_QuanlySVFile.java
@@ -1,8 +1,31 @@
 // Import các lớp
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Baitap3_QuanlySVFile {
 
 	public static void main(String[] args) {
-		
+		// Nhập thông tin các môn
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("=== NHẬP THÔNG TIN SINH VIÊN ===");
+        System.out.print("Mã sinh viên: ");
+        String id = scanner.nextLine();
+        
+        System.out.print("Họ tên: ");
+        String name = scanner.nextLine();
+        
+        System.out.print("Điểm Toán: ");
+        double math = scanner.nextDouble();
+        
+        System.out.print("Điểm Lý: ");
+        double physics = scanner.nextDouble();
+        
+        System.out.print("Điểm Hóa: ");
+        double chemistry = scanner.nextDouble();
+        
+        double average = (math + physics + chemistry) / 3;
 
 	}
 
