6fecfa32bdf1166ec2ea2416488c77bcd0ded485
trunglmtech <0602trungle95@gmail.com>
2026-03-11 15:05:57 +0700

    Dùng Set để gán số lượng mới

--- SanPhamVoiEncapsulation/src/MainSanPham.java
+++ SanPhamVoiEncapsulation/src/MainSanPham.java
@@ -1,5 +1,9 @@
 public class MainSanPham {
     public static void main(String[] args) {
-        SanPham sp = new SanPham("SP01", "Laptop", 15000, 2);
+        SanPham sp = new SanPham("1", "Laptop", 150, 1);
         sp.hienThiThongTin();
+        System.out.println("\nCập nhật số lượng");
+        sp.setSoLuong(5);
+        sp.hienThiThongTin();
+    }
 }
