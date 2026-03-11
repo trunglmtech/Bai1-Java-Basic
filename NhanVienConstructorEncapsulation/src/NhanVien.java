28ad9dc4f1b35b711de38c0568b485ad0739c833
trunglmtech <0602trungle95@gmail.com>
2026-03-11 15:13:53 +0700

    Constructor chứa tham số

--- NhanVienConstructorEncapsulation/src/NhanVien.java
+++ NhanVienConstructorEncapsulation/src/NhanVien.java
@@ -3,4 +3,10 @@ public class NhanVien {
     private String hoTen;
     private double luongCoBan;
     private double heSoLuong;
+    // Constructor tham số
+    public NhanVien(String maNV, String hoTen, double luongCoBan, double heSoLuong) {
+        this.maNV = maNV;
+        this.hoTen = hoTen;
+        this.luongCoBan = luongCoBan;
+        this.heSoLuong = heSoLuong;
 }
