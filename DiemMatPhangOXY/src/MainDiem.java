9012aa46e59892c85abaa1f70d0c8529d97519f7
trunglmtech <0602trungle95@gmail.com>
2026-03-11 15:32:40 +0700

    Kiểm tra trùng nhau không

--- DiemMatPhangOXY/src/MainDiem.java
+++ DiemMatPhangOXY/src/MainDiem.java
@@ -3,6 +3,15 @@ public class MainDiem {
 
         Diem d1 = new Diem(3, 4);
         Diem d2 = new Diem(6, 8);
-    // Tính khoảng cách
+        // Tính khoảng cách
         System.out.println("Khoảng cách: " + d1.tinhKhoangCach(d2));
+
+
+        // Kiểm tra trùng nhau
+        if (d1.kiemTraTrungNhau(d2)) {
+            System.out.println("Hai điểm trùng nhau");
+        } else {
+            System.out.println("Hai điểm không trùng nhau");
+        }
     }
+}
