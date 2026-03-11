8b83e05b4f42358e3de3d2e4aaaa96bd363bc6e6
trunglmtech <0602trungle95@gmail.com>
2026-03-11 15:27:46 +0700

    Khai báo thuộc tính và constructor mặc định

--- DiemMatPhangOXY/src/Diem.java
+++ DiemMatPhangOXY/src/Diem.java
@@ -1,3 +1,10 @@
 public class Diem {
-    
+    private double x;
+    private double y;
+
+    // Constructor mặc định (0,0)
+    public Diem() {
+        this.x = 0;
+        this.y = 0;
+    }
 }
