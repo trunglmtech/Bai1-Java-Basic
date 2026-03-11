97b01a3c7bb8c5df46758a28ae560d6d7e81dd48
trunglmtech <0602trungle95@gmail.com>
2026-03-11 15:20:42 +0700

    Thử dùng setter đổi thông tin

--- NhanVienConstructorEncapsulation/src/MainNhanVien.java
+++ NhanVienConstructorEncapsulation/src/MainNhanVien.java
@@ -7,6 +7,10 @@ public class MainNhanVien {
         System.out.println("Họ tên: " + nv.getHoTen());
         System.out.println("Lương: " + nv.tinhLuong());
         System.out.println("Xếp loại: " + nv.xepLoai());
-
+    // Đổi thông tin
+    nv.setHoTen("Nguyen Van ABC");
+    nv.setLuongCoBan(2000);
+        System.out.println("Họ tên: " + nv.getHoTen());
+        System.out.println("Lương: " + nv.tinhLuong());
     }
 }
