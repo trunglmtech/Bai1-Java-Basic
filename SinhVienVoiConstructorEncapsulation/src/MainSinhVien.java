49cf5184f39d851e0096e7b66cac25d28fec875f
trunglmtech <0602trungle95@gmail.com>
2026-03-11 14:38:47 +0700

    Tạo object sinh viên với constructor mặc định

--- SinhVienVoiConstructorEncapsulation/src/MainSinhVien.java
+++ SinhVienVoiConstructorEncapsulation/src/MainSinhVien.java
@@ -1,2 +1,12 @@
 public class MainSinhVien {
+    public static void main(String[] args) {
+        // Tạo sinh viên với constructor mặc định
+        SinhVien sv1 = new SinhVien();
+        sv1.setTen("Nguyễn Văn A");
+        sv1.setTuoi(20);
+        sv1.setMaSV("SV001");
+        sv1.setDiemTB(8.5);
+        sv1.hienThiThongTin();
+
+        System.out.println("\n---\n");
 }
