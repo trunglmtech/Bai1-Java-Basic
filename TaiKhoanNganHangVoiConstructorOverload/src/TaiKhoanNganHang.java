b8ef40c4323f555f0b4f3948766463cf93613b6c
trunglmtech <0602trungle95@gmail.com>
2026-03-11 14:47:42 +0700

    Khai báo thuộc tính và các hàm constructor

--- TaiKhoanNganHangVoiConstructorOverload/src/TaiKhoanNganHang.java
+++ TaiKhoanNganHangVoiConstructorOverload/src/TaiKhoanNganHang.java
@@ -1,2 +1,26 @@
 public class TaiKhoanNganHang {
+    private String soTaiKhoan;
+    private String tenChuTaiKhoan;
+    private double soDu;
+
+    // Constructor 1: Chỉ có số tài khoản
+    public TaiKhoanNganHang(String soTaiKhoan) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.tenChuTaiKhoan = "Chưa xác định";
+        this.soDu = 0.0;
+    }
+
+    // Constructor 2: Có số tài khoản và tên chủ
+    public TaiKhoanNganHang(String soTaiKhoan, String tenChuTaiKhoan) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.tenChuTaiKhoan = tenChuTaiKhoan;
+        this.soDu = 0.0;
+    }
+
+    // Constructor 3: Đầy đủ thông tin
+    public TaiKhoanNganHang(String soTaiKhoan, String tenChuTaiKhoan, double soDu) {
+        this.soTaiKhoan = soTaiKhoan;
+        this.tenChuTaiKhoan = tenChuTaiKhoan;
+        this.soDu = (soDu >= 0) ? soDu : 0.0;
+    }
 }
