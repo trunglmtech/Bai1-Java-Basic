b1790d7bb74a4194870fa5b7405e9b8518371f4a
trunglmtech <0602trungle95@gmail.com>
2026-03-11 14:59:32 +0700

    Constructor mặc định

--- SanPhamVoiEncapsulation/src/SanPham.java
+++ SanPhamVoiEncapsulation/src/SanPham.java
@@ -3,4 +3,11 @@ public class SanPham {
     private String tenSP;
     private double gia;
     private int soLuong;
+    // Constructor mặc định
+    public SanPham() {
+        this.maSP = "Chưa có";
+        this.tenSP = "Chưa rõ";
+        this.gia = 1;
+        this.soLuong = 0;
+    }
 }
