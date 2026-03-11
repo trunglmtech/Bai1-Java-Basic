1e366f10d7b12c9994cb8899113a7e8b9f22455b
trunglmtech <0602trungle95@gmail.com>
2026-03-11 15:10:51 +0700

    Test main hoạt động

--- SanPhamVoiEncapsulation/src/MainSanPham.java
+++ SanPhamVoiEncapsulation/src/MainSanPham.java
@@ -5,5 +5,10 @@ public class MainSanPham {
         System.out.println("\nCập nhật số lượng");
         sp.setSoLuong(5);
         sp.hienThiThongTin();
+        System.out.println("\n");
+        sp.setTenSP("IPHONE 20000 PRO MAX");
+        sp.setGia(30000000);
+        sp.setSoLuong(9000000);
+        sp.hienThiThongTin();
     }
 }
