af172bee42a5c01ff6fbe368dc020f8ea3743807
Lê Minh Trung <0602trungle95@gmail.com>
2026-03-17 15:16:42 +0700

    Nhập các số từ bàn phím

--- ArrayListOOP/src/runMain.java
+++ ArrayListOOP/src/runMain.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
-    
+    public static void main(String[] args){
+        ArrayList<Integer> dssonguyen = new ArrayList<Integer>();
+        Scanner banphim = new Scanner(System.in);
+
+        for(int i = 0 ; i< n; i++){
+            System.out.println("Moi ban nhap phan tu thu " + i + ": ");
+            Integer tam = banphim.nextInt();
+            dssonguyen.add(tam);
+            
+        }
+    }
 }
