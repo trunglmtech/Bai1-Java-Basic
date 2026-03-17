0e55a8e1c7a095a02da48c32383177f1a9a88562
Lê Minh Trung <0602trungle95@gmail.com>
2026-03-17 15:22:14 +0700

    In ra màn hình

--- ArrayListOOP/src/runMain.java
+++ ArrayListOOP/src/runMain.java
@@ -6,11 +6,16 @@ public class runMain {
         ArrayList<Integer> dssonguyen = new ArrayList<Integer>();
         Scanner banphim = new Scanner(System.in);
 
-        for(int i = 0 ; i< n; i++){
-            System.out.println("Moi ban nhap phan tu thu " + i + ": ");
+        for(int i = 0 ; i< 10; i++){
+            System.out.print("Moi ban nhap phan tu thu " + i + ": ");
             Integer tam = banphim.nextInt();
             dssonguyen.add(tam);
-            
+
+        }
+        // b. in ra màn hình
+        System.out.print("Danh sach so nguyen la: ");
+        for ( Integer x : dssonguyen){
+            System.out.print(x + " ");
         }
     }
 }
