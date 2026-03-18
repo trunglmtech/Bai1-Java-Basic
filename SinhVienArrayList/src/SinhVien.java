a2424068c26e322c037c961581b92d8d5db2cc92
trunglmtech <0602trungle95@gmail.com>
2026-03-18 17:48:08 +0700

    tạo class qlsv arraylist

--- SinhVienArrayList/src/SinhVien.java
+++ SinhVienArrayList/src/SinhVien.java
@@ -1,2 +1,21 @@
 public class SinhVien {
+    private String maSV;
+    private String hoTen;
+    private double diemTB;
+
+    public SinhVien(String maSV, String hoTen, double diemTB) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diemTB = diemTB;
+    }
+
+    // Getter
+    public String getMaSV() { return maSV; }
+    public String getHoTen() { return hoTen; }
+    public double getDiemTB() { return diemTB; }
+
+    @Override
+    public String toString() {
+        return String.format("Mã:
+    }
 }
