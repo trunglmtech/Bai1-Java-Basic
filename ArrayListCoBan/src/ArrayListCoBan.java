6ba144063e3bec7baf94d648e167dc2bee4652d0
trunglmtech <0602trungle95@gmail.com>
2026-03-18 17:39:11 +0700

    Thêm phần tử - add()

--- ArrayListCoBan/src/ArrayListCoBan.java
+++ ArrayListCoBan/src/ArrayListCoBan.java
@@ -2,8 +2,14 @@ import java.util.List;
 
 public class ArrayListCoBan {
     public static void main(String[] args) {
-        System.out.println("=== ARRAYLIST CƠ BẢN - MINH HỌA CÁC PHƯƠNG THỨC ===\n");
+        System.out.println(" ARRAYLIST CƠ BẢN - MINH HỌA CÁC PHƯƠNG THỨC \n");
 
         // 1. Khai báo ArrayList với Generics
         List<String> danhSachTen = new ArrayList<>();
-}
+        // 2. Thêm phần tử - add()
+        System.out.println("--- THÊM PHẦN TỬ ---");
+        danhSachTen.add("Nguyễn Văn A");
+        danhSachTen.add("Trần Thị B");
+        danhSachTen.add("Lê Văn C");
+
+    }
