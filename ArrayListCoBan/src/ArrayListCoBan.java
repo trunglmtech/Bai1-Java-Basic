1c4260f16bc6e285f3f12caaf311b97e974fc958
trunglmtech <0602trungle95@gmail.com>
2026-03-18 17:38:36 +0700

    Khai báo ArrayList với Generics

--- ArrayListCoBan/src/ArrayListCoBan.java
+++ ArrayListCoBan/src/ArrayListCoBan.java
@@ -1,3 +1,9 @@
+import java.util.List;
+
 public class ArrayListCoBan {
-    
+    public static void main(String[] args) {
+        System.out.println("=== ARRAYLIST CƠ BẢN - MINH HỌA CÁC PHƯƠNG THỨC ===\n");
+
+        // 1. Khai báo ArrayList với Generics
+        List<String> danhSachTen = new ArrayList<>();
 }
