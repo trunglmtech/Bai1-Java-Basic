87dfec6005984b485278e95280f4c874a9e8fadc
trunglmtech <0602trungle95@gmail.com>
2026-03-18 17:51:00 +0700

    Lọc sinh viên

--- SinhVienArrayList/src/QuanLySinhVien.java
+++ SinhVienArrayList/src/QuanLySinhVien.java
@@ -50,5 +50,31 @@ public double tinhDiemTrungBinhLop() {
         tong += sv.getDiemTB();
     }
     return tong / danhSachSV.size();
-}
+    }
+    // Lọc sinh viên có điểm >= điểm cho trước
+    public List<SinhVien> locSinhVienTheoDiem(double diemToiThieu) {
+        List<SinhVien> ketQua = new ArrayList<>();
+        for (SinhVien sv : danhSachSV) {
+            if (sv.getDiemTB() >= diemToiThieu) {
+                ketQua.add(sv);
+            }
+        }
+        return ketQua;
+    }
+
+    // Sắp xếp theo điểm (giảm dần) - sử dụng thuật toán đơn giản
+    public void sapXepTheoDiem() {
+        // Sử dụng bubble sort
+        for (int i = 0; i < danhSachSV.size() - 1; i++) {
+            for (int j = 0; j < danhSachSV.size() - i - 1; j++) {
+                if (danhSachSV.get(j).getDiemTB() < danhSachSV.get(j + 1).getDiemTB()) {
+                    // Hoán đổi
+                    SinhVien temp = danhSachSV.get(j);
+                    danhSachSV.set(j, danhSachSV.get(j + 1));
+                    danhSachSV.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
 }
