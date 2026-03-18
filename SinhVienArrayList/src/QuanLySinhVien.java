f9a454038a477e3919bc114dc1de8e5779f2647c
trunglmtech <0602trungle95@gmail.com>
2026-03-18 17:49:45 +0700

    Thêm sinh viên , tìm sinh viên

--- SinhVienArrayList/src/QuanLySinhVien.java
+++ SinhVienArrayList/src/QuanLySinhVien.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuanLySinhVien {
+    private List<SinhVien> danhSachSV;
+
+    public QuanLySinhVien() {
+        danhSachSV = new ArrayList<>();
+    }
+
+    // Thêm sinh viên - add()
+    public void themSinhVien(SinhVien sv) {
+        danhSachSV.add(sv);
+        System.out.println("Đã thêm: " + sv.getHoTen());
+    }
+
+    // Tìm sinh viên theo mã - sử dụng get() và vòng lặp
+    public SinhVien timTheoMa(String maSV) {
+        for (SinhVien sv : danhSachSV) {
+            if (sv.getMaSV().equals(maSV)) {
+                return sv;
+            }
+        }
+        return null;
+    }
 }
--- SinhVienArrayList/src/SinhVien.java
+++ SinhVienArrayList/src/SinhVien.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SinhVien {
     private String maSV;
     private String hoTen;
