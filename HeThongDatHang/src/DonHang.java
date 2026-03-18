993e179d2786fe87e3c658787968548d0690c3ae
trunglmtech <0602trungle95@gmail.com>
2026-03-18 18:19:31 +0700

    Tạo class DonHang

--- HeThongDatHang/src/DonHang.java
+++ HeThongDatHang/src/DonHang.java
@@ -1,2 +1,24 @@
+import java.util.List;
+
 public class DonHang {
+    String maDH;
+    String tenKH;
+    List<String> danhSachSanPham;
+    double tongTien;
+    String ngayDat; // "dd-MM-yyyy"
+
+    public DonHang(String maDH, String tenKH, List<String> danhSachSanPham, double tongTien, String ngayDat) {
+        this.maDH = maDH;
+        this.tenKH = tenKH;
+        this.danhSachSanPham = danhSachSanPham;
+        this.tongTien = tongTien;
+        this.ngayDat = ngayDat;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MaDH: %s | KH: %s | SanPham: %s | TongTien: %.2f | Ngay: %s",
+                maDH, tenKH, danhSachSanPham, tongTien, ngayDat);
+    }
+
 }
