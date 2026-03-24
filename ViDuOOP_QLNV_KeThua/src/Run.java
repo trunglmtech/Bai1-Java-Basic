f73eda0b9cabda6be8d810938b9092a3c838f333
Lê Minh Trung <0602trungle95@gmail.com>
2026-03-24 14:07:07 +0700

    tạo lớp nhân viên văn phòng

--- ViDuOOP_QLNV_KeThua/src/Run.java
+++ ViDuOOP_QLNV_KeThua/src/Run.java
@@ -1,2 +1,13 @@
 public class Run {
+
+    public static void main(String[] args) {
+        // Test lớp Nhân Viên
+        NhanVien nv1 = new NhanVien("001", "Nguyễn Ngọc Yến Trang", 2300000);
+        // nv1.hienThiThongTin();
+        System.out.println(nv1.toString());
+        NhanVienVanPhong nvVanPhong1 = new NhanVienVanPhong("002", "Võ Thị Thanh Ngân", 2300000, 3, 200000);
+        System.out.println(nvVanPhong1.toString());
+        //	nvVanPhong1.hienThiThongTin();
+    }
+
 }
