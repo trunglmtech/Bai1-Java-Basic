1fd60e1b3183aa82d85a34a1a9083884faea7ad8
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-01 22:19:59 +0700

    Tạo các constructor và getter setter

--- QuanLyHocSinh/src/trunglmtech/HocSinh.java
+++ QuanLyHocSinh/src/trunglmtech/HocSinh.java
@@ -1,5 +1,55 @@
 package trunglmtech;
 
 public class HocSinh {
+	
+	    // Khai báo các thuộc tính private
+	    private String tenHocSinh;
+	    private int tuoiHocSinh; 
+	    private String lopHocSinh;
 
-}
+	    // Hàm tạo (Constructor) không tham số
+	    public HocSinh() {
+	    }
+
+	    // Hàm tạo (Constructor) có đầy đủ tham số
+	    public HocSinh(String tenHocSinh, int tuoiHocSinh, String lopHocSinh) {
+	        this.tenHocSinh = tenHocSinh;
+	        this.tuoiHocSinh = tuoiHocSinh;
+	        this.lopHocSinh = lopHocSinh;
+	    }
+
+	    // --- Các phương thức Getters và Setters ---
+	    public String getTenHocSinh() {
+	        return tenHocSinh;
+	    }
+
+	    public void setTenHocSinh(String tenHocSinh) {
+	        this.tenHocSinh = tenHocSinh;
+	    }
+
+	    public int getTuoiHocSinh() {
+	        return tuoiHocSinh;
+	    }
+
+	    public void setTuoiHocSinh(int tuoiHocSinh) {
+	        this.tuoiHocSinh = tuoiHocSinh;
+	    }
+
+	    public String getLopHocSinh() {
+	        return lopHocSinh;
+	    }
+
+	    public void setLopHocSinh(String lopHocSinh) {
+	        this.lopHocSinh = lopHocSinh;
+	    }
+
+	    // Tùy biến phương thức toString() để in ra thông tin đẹp mắt
+	    @Override
+	    public String toString() {
+	        String chuoiXuat = "Họ và tên: " + tenHocSinh + "\n" +
+	                           "Tuổi: " + tuoiHocSinh + "\n" +
+	                           "Lớp: " + lopHocSinh;
+	        return chuoiXuat;
+	    }
+	}
+
