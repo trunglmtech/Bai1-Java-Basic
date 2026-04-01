08b95eff8879e04ef981afbc0a9afd78d90e363a
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-01 22:21:50 +0700

    Tạo học sinh hardcore

--- QuanLyHocSinh/src/trunglmtech/Run.java
+++ QuanLyHocSinh/src/trunglmtech/Run.java
@@ -3,7 +3,15 @@ package trunglmtech;
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        // --- CÂU 2A: TẠO MỚI HỌC SINH (HARDCODE DỮ LIỆU) ---
+        System.out.println("--- 2A: KHỞI TẠO CÓ SẴN DỮ LIỆU ---");
+        // Cách 1: Dùng hàm tạo không tham số và Setter
+        HocSinh hs1 = new HocSinh();
+        hs1.setTenHocSinh("Trần Văn A");
+        hs1.setTuoiHocSinh(10);
+        hs1.setLopHocSinh("8A12");
 
 	}
 
