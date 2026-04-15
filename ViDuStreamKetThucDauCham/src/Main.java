f1f40618c213322a958d0f1228c56af1a7c57b1b
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-15 20:29:45 +0700

    Nhập ký tự kết thúc bằng dấu chấm

--- ViDuStreamKetThucDauCham/src/Main.java
+++ ViDuStreamKetThucDauCham/src/Main.java
@@ -1,8 +1,19 @@
 
+import java.io.*; 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		{
+			char c;
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Nhap chuoi ky tu, gioi han dau cham.");
+			 // read characters
+			do
+			{
+			c = (char) br.read();
+			System.out.print(c);
+			} while(c != '.');
+			} 
 
 	}
 
