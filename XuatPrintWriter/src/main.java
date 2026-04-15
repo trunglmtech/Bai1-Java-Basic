cd280bc9f444acf0dbaaf1bfdb3c177d985e521f
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-15 20:34:02 +0700

    Dùng printwrite như systemoutprintline

--- XuatPrintWriter/src/main.java
+++ XuatPrintWriter/src/main.java
@@ -1,9 +1,19 @@
-
+import java.io.*;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		PrintWriter pw = new PrintWriter(System.out, true);
 
-	}
+        int i = 10;
+        double d = 123.67;
+        double r = i + d;
 
+        pw.println("dùng PrintWriter:");
+        pw.println("i = " + i);
+        pw.println("d = " + d);
+        pw.println(i + " + " + d + " = " + r);
+    }
 }
+
+
