5e3e496211fa4b7efb057e6cca2c03732b6b8132
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-23 07:36:00 +0700

    LẦN THỨ 1

--- CAU1THIGIUAKI/src/thigk2/leminhtrung/Cau1.java
+++ CAU1THIGIUAKI/src/thigk2/leminhtrung/Cau1.java
@@ -2,6 +2,18 @@ package thigk2.leminhtrung;
 
 public class Cau1 {
 
+	/**
+     * TÓM TẮT CÁCH GIẢI:
+     * 1. Sử dụng Scanner để nhận tọa độ x, y của 2 điểm Trên-Trái và Dưới-Phải từ bàn phím.
+     * 2. Tính chiều dài và chiều rộng bằng cách lấy trị tuyệt đối hiệu các tọa độ x và y tương ứng:
+     * Chiều rộng = |x1 - x2|, Chiều dài = |y1 - y2|.
+     * 3. Áp dụng công thức: Chu vi = (Dài + Rộng) * 2; Diện tích = Dài * Rộng.
+     * 4. In kết quả ra màn hình.
+     */
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
