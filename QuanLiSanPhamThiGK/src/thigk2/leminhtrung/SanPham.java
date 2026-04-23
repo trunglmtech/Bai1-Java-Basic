7b5b47dc201c5ef57609d4bef96f9a7edaf4ec0a
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-23 07:47:02 +0700

    LẦN THỨ 1

--- QuanLiSanPhamThiGK/src/thigk2/leminhtrung/SanPham.java
+++ QuanLiSanPhamThiGK/src/thigk2/leminhtrung/SanPham.java
@@ -1,5 +1,12 @@
 package thigk2.leminhtrung;
 
+/**
+ * Lớp sản phẩm bao gồm các thông tin cơ bản và thuộc tính giá tự thêm.
+ */
 public class SanPham {
-
+	
+	    private String maSP;
+	    private String tenSP;
+	    private String loaiSP;
+	    private double gia; // Thuộc tính thứ 4 tự thêm theo yêu cầu
 }
