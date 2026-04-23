7af10d4f73384c4fbf01acce7b293c2cd3a054ab
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-23 08:05:13 +0700

    LẦN THỨ 7

--- DocFileThiGK/src/thigk2/leminhtrung/main.java
+++ DocFileThiGK/src/thigk2/leminhtrung/main.java
@@ -1,9 +1,21 @@
 package thigk2.leminhtrung;
 
 public class main {
+	
+	/**
+     * TÓM TẮT CÁCH GIẢI:
+     * 1. Sử dụng lớp QuanLySoThuc để quản lý danh sách số thực bằng ArrayList.
+     * 2. Gọi phương thức docDuLieuTuFile với đường dẫn tuyệt đối đến Desktop để lấy 80 số.
+     * 3. Sử dụng vòng lặp và phương thức get() để in danh sách ra màn hình theo định dạng dễ nhìn.
+     * 4. Sử dụng phương thức contains() của ArrayList để kiểm tra sự tồn tại của ngày sinh X.
+     */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	uanLySoThuc quanLy = new QuanLySoThuc();
+        
+        // ĐƯỜNG DẪN ĐẾN FILE TRÊN DESKTOP CỦA BẠN
+        // Giả sử tên file là "dulieu.txt", bạn hãy sửa lại đúng tên file thực tế nhé
+        String path = "C:\\Users\\Trung\\Desktop\\dulieu.txt";
 
 	}
 
