374fb0c1a47f4d0a13c02ea6bd3922cebfc0ec08
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-23 08:00:26 +0700

    LẦN THỨ 1

---
+++ DocFileThiGK/src/thigk2/leminhtrung/QuanLiSoThuc.java
@@ -0,0 +1,15 @@
+package thigk2.leminhtrung;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class QuanLiSoThuc {
+	
+	private ArrayList<Double> danhSachSo;
+
+    public QuanLySoThuc() {
+        this.danhSachSo = new ArrayList<>();
+    }
+}
