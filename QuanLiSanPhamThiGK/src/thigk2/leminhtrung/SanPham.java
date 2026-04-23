dd56f2875b6c011ffde269a857e2e40344c6f5e3
Lê Minh Trung <0602trungle95@gmail.com>
2026-04-23 07:47:32 +0700

    LẦN THỨ 2

--- QuanLiSanPhamThiGK/src/thigk2/leminhtrung/SanPham.java
+++ QuanLiSanPhamThiGK/src/thigk2/leminhtrung/SanPham.java
@@ -9,4 +9,13 @@ public class SanPham {
 	    private String tenSP;
 	    private String loaiSP;
 	    private double gia; // Thuộc tính thứ 4 tự thêm theo yêu cầu
+	    
+	    
+	 // Constructor đầy đủ tham số
+	    public SanPham(String maSP, String tenSP, String loaiSP, double gia) {
+	        this.maSP = maSP;
+	        this.tenSP = tenSP;
+	        this.loaiSP = loaiSP;
+	        this.gia = gia;
+	    }
 }
